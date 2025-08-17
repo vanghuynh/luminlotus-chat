34e174a1b7bb31a89f480555bfb83533b67e02c3
ChuongLV1 <lvchuong003@gmail.com>
2025-08-17 16:05:42 +0700

    fix app.py

--- app.py
+++ app.py
@@ -52,6 +52,10 @@ def chat():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+application = app
 
 # # Route xử lý chat
 # @app.route("/chat", methods=["POST"])
